0b4d5240671840cbb81be0ee8cf27a3c66913480
Steffen Baer <stbaer.mail@gmail.com>
2017-01-17 23:15:08 +0800

    fix: commit links #2; updated HISTORY.md

--- bin/history.js
+++ bin/history.js
@@ -12,6 +12,7 @@ args.option('dry-run', 'only log commands without executing them', false);
 args.option('out', 'output file', 'History.md');
 args.option('exclude-strings', 'excludes a commit if its title contains this string');
 args.option('include-strings', 'excludes a commit if its title contains this string');
+args.option('commit-base-url', 'set the remote url for linking to commits, defaults to git-remote-url/commit');
 
 const flags = args.parse(process.argv);
 const file = flags.o || 'History.md';
@@ -21,7 +22,8 @@ flags.i && (includeStrings = util.isArray(flags.i) ? flags.i : [flags.i]);
 flags.e && (excludeStrings = util.isArray(flags.e) ? flags.e : [flags.e]);
 
 remoteUrl( (err, url) => {
-    getHistoryMd({excludeStrings, includeStrings, url})
+    console.log(url);
+    getHistoryMd({excludeStrings, includeStrings, url: (flags.c || url + 'commit')})
         .then((str) => {
             file && shellEx(`echo "${str}\n\n" > ${file}`, null, null, flags.d);
         });

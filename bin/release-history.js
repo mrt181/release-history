7c2cfc6c0acc8c21cdab6089ba0c65ac589cf19f
Steffen Baer <stbaer.mail@gmail.com>
2017-01-18 18:55:59 +0800

    only include commit links if a commit base url is set

--- bin/release-history.js
+++ bin/release-history.js
@@ -1,7 +1,6 @@
 #! /usr/bin/env node
 
 const util = require('util');
-const remoteUrl = require('remote-origin-url');
 const args = require('args');
 
 const getHistoryMd = require('./../lib/get-history-md');
@@ -12,7 +11,7 @@ args.option('dry-run', 'only log commands without executing them', false);
 args.option('out', 'output file', 'History.md');
 args.option('exclude-strings', 'excludes a commit if its title contains this string');
 args.option('include-strings', 'excludes a commit if its title contains this string');
-args.option('commit-base-url', 'set the remote url for linking to commits, defaults to git-remote-url/commit');
+args.option('commit-base-url', 'set the remote url for linking to commits');
 
 const flags = args.parse(process.argv);
 const file = flags.o || 'History.md';
@@ -22,9 +21,7 @@ let excludeStrings;
 flags.i && (includeStrings = util.isArray(flags.i) ? flags.i : [flags.i]);
 flags.e && (excludeStrings = util.isArray(flags.e) ? flags.e : [flags.e]);
 
-remoteUrl((err, url) => {
-    getHistoryMd({excludeStrings, includeStrings, url: (flags.c || url + 'commit')}).then(str => {
-        file && shellEx(`echo "${str}\n\n" > ${file}`, null, null, flags.d);
-    });
+getHistoryMd({excludeStrings, includeStrings, url: (flags.c)}).then(str => {
+    file && shellEx(`echo "${str}\n\n" > ${file}`, null, null, flags.d);
 });
 
